547264f935ba7818adcac6d832f25d8d1098b34a
Andrew Davisson <akdavisson4@gmail.com>
2020-05-13 12:12:37 -0600

    v-oncev-html

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ new Vue({
   el: '#app',
   data: {
     title: 'Hello World!',
-    link: 'https://google.com'
+    link: 'https://google.com',
+    finishedLink: '<a href="https://google.com">Google</a>',
   },
   methods: {
     sayHello: function() {
+      this.title = 'Hello!'
       return this.title
     },
   },
